1aa5aa6884aaf07fc013fce0f8925cd67e1e5e94
twittner <twittner@pld-linux.org>
2004-07-28 19:02:37 +0000

    - allow to pass flags & parameters for ssh-agent via ssh_agent_flags. - read system-wide configuration from /etc/ssh/ssh-agent.conf - changes not tested yet.
    Changed files:
        ssh-agent.sh -> 1.2

--- ssh-agent.sh
+++ ssh-agent.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
-# ssh-agent.sh - linux ver. (make against procps-3.2.1), sh/bash/zsh compatible
-# like gnupg-agent-agent.sh (what the long & insane name!?) put this
-# in /etc/profile.d/ chmod 755 /etc/profile.d/ssh-agent.sh
+# ssh-agent.sh sh/bash/zsh compatible script for /etc/profile.d/ .
+# Works like gnupg-agent-agent.sh . Copy this in /etc/profile.d/ ,
 # make ln -s /etc/profile.d/ssh-agent.sh /etc/X11/xinit/xinitrc.d/ssh-agent.sh
-# echo "ssh_agent_enable=yes" > $HOME/.ssh/ssh-agent.conf and forget about keychain
+# run echo "ssh_agent_enable=yes" > $HOME/.ssh/ssh-agent.conf . 
 
-SSH_AGENT_CONF="${HOME}/.ssh/ssh-agent.conf"
+[ -f /etc/ssh/ssh-agent.conf ] && SSH_AGENT_CONF="/etc/ssh/ssh-agent.conf"
+[ -f "${HOME}/.ssh/ssh-agent.conf" ] && SSH_AGENT_CONF="${HOME}/.ssh/ssh-agent.conf"
 if [ -s "$SSH_AGENT_CONF" ] ; then
 	. "$SSH_AGENT_CONF" || :
 	if [ "$ssh_agent_enable" = "yes" -o "$ssh_agent_enable" = "YES" ] ; then
@@ -13,11 +13,11 @@ if [ -s "$SSH_AGENT_CONF" ] ; then
 		if [ -s "$SSH_AGENT_DATA" ] ; then
         		. "$SSH_AGENT_DATA" > /dev/null
 		        if [ "$(ps -p "$SSH_AGENT_PID" | tail -n1 | awk '{print $4}')" != "ssh-agent" ] ; then
-        		        ssh-agent > "$SSH_AGENT_DATA" 2>&1
+        		        ssh-agent "$ssh_agent_flags" > "$SSH_AGENT_DATA" 2>&1
 		                . "$SSH_AGENT_DATA" > /dev/null
 		        fi
 		else
-		        ssh-agent > "$SSH_AGENT_DATA" 2>&1
+		        ssh-agent "$ssh_agent_flags" > "$SSH_AGENT_DATA" 2>&1
 		        . "$SSH_AGENT_DATA" > /dev/null
 		fi
 	fi
